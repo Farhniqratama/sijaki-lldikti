d18befda6385ec5efca928058bf9d71af8addeba
Farhniqratama <farhniqratama@gmail.com>
2025-01-10 10:25:37 +0700

    sijaki-lldikti
    Sistem Informasi Jejak Perguruan Tinggi Lembaga Layanan Pendidikan Tinggi Wilayah III Jakarta

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'ADMINKLK',
+            'pokja' => 'Admin SI-JAKI',
+            'akses' => 'Admin',
+            'password' => Hash::make('lldikti3'),
+        ]);
+        User::create([
+            'name' => 'KLK',
+            'pokja' => 'Kelembagaan dan Kemitraan',
+            'akses' => 'User',
+            'password' => Hash::make('lldikti3'),
+        ]);
+
+        User::create([
+            'name' => 'SI',
+            'pokja' => 'Sistem Informasi dan PDDikti',
+            'akses' => 'User',
+            'password' => Hash::make('lldikti3'),
+        ]);
+
+        User::create([
+            'name' => 'HUMAS',
+            'pokja' => 'Hubungan Masyarakat dan Kerja Sama',
+            'akses' => 'User',
+            'password' => Hash::make('lldikti3'),
+        ]);
+
+        User::create([
+            'name' => 'HKT',
+            'pokja' => 'Hukum, Kepegawaian, dan Tata Laksana',
+            'akses' => 'User',
+            'password' => Hash::make('lldikti3'),
+        ]);
+
+        User::create([
+            'name' => 'RPM',
+            'pokja' => 'Riset dan Pengabdian Masyarakat',
+            'akses' => 'User',
+            'password' => Hash::make('lldikti3'),
+        ]);
+
+        User::create([
+            'name' => 'BELMAWA',
+            'pokja' => 'Pembelajaran, Kemahasiswaan, dan Prestasi',
+            'akses' => 'User',
+            'password' => Hash::make('lldikti3'),
+        ]);
+
+        User::create([
+            'name' => 'PENJAMU',
+            'pokja' => 'Penjaminan Mutu',
+            'akses' => 'User',
+            'password' => Hash::make('lldikti3'),
+        ]);
+
+        User::create([
+            'name' => 'SDP',
+            'pokja' => 'Sumber Daya',
+            'akses' => 'User',
+            'password' => Hash::make('lldikti3'),
+        ]);
+
+        User::create([
+            'name' => 'ADIA',
+            'pokja' => 'Anti Dosa Pendidikan dan Integritas Akademik',
+            'akses' => 'User',
+            'password' => Hash::make('lldikti3'),
+        ]);
+
+        User::create([
+            'name' => 'PP',
+            'pokja' => 'Perencanaan dan Keuangan',
+            'akses' => 'User',
+            'password' => Hash::make('lldikti3'),
+        ]);
+
+        User::create([
+            'name' => 'TUBMN',
+            'pokja' => 'Tata Usaha dan Barang Milik Negara',
+            'akses' => 'User',
+            'password' => Hash::make('lldikti3'),
+        ]);
+    }
+}
